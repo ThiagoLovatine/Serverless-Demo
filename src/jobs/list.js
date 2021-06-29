34db1eab25e8077a1bad9915f5ad844bdc85e9ea
Thiago Lovatine <thiagolovatine@gmail.com>
2021-06-29 14:02:22 -0300

    List jobs DynamoDB

--- src/jobs/list.js
+++ src/jobs/list.js
@@ -1,12 +1,20 @@
+const AWS = require("aws-sdk");
+
+const DynamoDB = new AWS.DynamoDB.DocumentClient({
+  region: "localhost",
+  endpoint: "http://localhost:8000",
+});
+
+
 module.exports.jobList = async (evt, ctx) => {
+  params = {
+    TableName: "JobsDBTable",
+    Limit: 10
+  }
+  const newJobResponse = await DynamoDB.scan(params).promise();
+
   return {
     code: 200,
-    body: JSON.stringify(
-      {
-        message: "List of jobs!",
-      },
-      null, // replacer
-      2 // space
-    ),
+    body: JSON.stringify(newJobResponse),
   };
 };
