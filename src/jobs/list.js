2c7afb193c2d27c8f0fd1fb3414b4aaad8f33f2f
Thiago Lovatine <thiagolovatine@gmail.com>
2021-06-29 15:15:11 -0300

    Fix bug on list prod

--- src/jobs/list.js
+++ src/jobs/list.js
@@ -1,18 +1,27 @@
 const AWS = require("aws-sdk");
 
-const options = process.env.IS_OFFLINE ? { region: "localhost", endpoint: "http://localhost:8000" } : {}
+const options = process.env.IS_OFFLINE
+  ? { region: "localhost", endpoint: "http://localhost:8000" }
+  : {};
 const DynamoDB = new AWS.DynamoDB.DocumentClient(options);
 
 module.exports.jobList = async (evt, ctx) => {
   params = {
     TableName: process.env.TABLE_JOBS,
-    Limit: 10
-  }
-  
-  const newJobResponse = await DynamoDB.scan(params).promise();
-
-  return {
-    code: 200,
-    body: JSON.stringify(newJobResponse),
+    Limit: 10,
   };
+
+  try {
+    const newJobResponse = await DynamoDB.scan(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(newJobResponse),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({message: 'Error fetching jobs'}),
+    };
+  }
 };
